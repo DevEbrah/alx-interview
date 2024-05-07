74eaa4aff822e4f9fd0c281e97c6bc6271c28ed1
DevEbrah <onyangohibrahim0@gmail.com>
2024-05-07 20:32:07 +0300

    Interview

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,21 +1,27 @@
 #!/usr/bin/python3
-"""
-0-island_perimeter
+"""Island Perimeter Problem
 """
 
 
 def island_perimeter(grid):
-        """ Returns perimeter of an island description in grid """
-            perimeter = 0
-                m = len(grid)
-                    n = len(grid[0])
+        """
+            Calculates the perimeter of the island described in grid
+                Args:
+                        grid: 2d list of integers containing 0(water) or 1(land)
+                            Return:
+                                    the perimeter of the island
+                                        """
 
-                        for i in range(m):
-                                    for j in range(n):
-                                                    if grid[i][j] == 1:
-                                                                        for x, y in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
-                                                                                                a, b = i + x, j + y
-                                                                                                                    # print(a, b)
-                                                                                                                                        if a >= m or b >= n or a < 0 or b < 0 or grid[a][b] == 0:
-                                                                                                                                                                    perimeter += 1
-                                                                                                                                                                        return perimeter
+                                            p = 0
+                                                for i in range(len(grid)):
+                                                            for j in range(len(grid[i])):
+                                                                            if (grid[i][j] == 1):
+                                                                                                if (i <= 0 or grid[i - 1][j] == 0):
+                                                                                                                        p += 1
+                                                                                                                                        if (i >= len(grid) - 1 or grid[i + 1][j] == 0):
+                                                                                                                                                                p += 1
+                                                                                                                                                                                if (j <= 0 or grid[i][j - 1] == 0):
+                                                                                                                                                                                                        p += 1
+                                                                                                                                                                                                                        if (j >= len(grid[i]) - 1 or grid[i][j + 1] == 0):
+                                                                                                                                                                                                                                                p += 1
+                                                                                                                                                                                                                                                    return p
